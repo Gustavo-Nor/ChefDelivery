e3bcd6e2744b36b349a686fb9a3b7ab3e65c37b3
Gustavo Nor <gustavo0951@gmail.com>
2024-03-26 19:39:23 -0300

    Stores view

---
+++ ChefDelivery/StoresView/StoreItemView.swift
@@ -0,0 +1,18 @@
+//
+//  StoreItemView.swift
+//  ChefDelivery
+//
+//  Created by Nor, Gustavo on 26/03/24.
+//
+
+import SwiftUI
+
+struct StoreItemView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StoreItemView()
+}
