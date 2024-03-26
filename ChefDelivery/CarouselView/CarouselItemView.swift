c000ac1bc2e33f1f7d8c81e4440210690f6bb2ca
Gustavo Nor <gustavo0951@gmail.com>
2024-03-26 19:14:40 -0300

    Carousel tab view

---
+++ ChefDelivery/CarouselView/CarouselItemView.swift
@@ -0,0 +1,18 @@
+//
+//  CarouselItemView.swift
+//  ChefDelivery
+//
+//  Created by Nor, Gustavo on 26/03/24.
+//
+
+import SwiftUI
+
+struct CarouselItemView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CarouselItemView()
+}
